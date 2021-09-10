3dbdb607526b1e80fb60fd427fc34c1ad61a1d71
Jenny Chau <jnechau@gmail.com>
2021-09-10 18:19:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+/* for ([initialization]; [condition]; [iteration]) {
+    [loop body]
+  }
+*/
+
+const hobbits = ["Frodo", "Sam", "Merry", "Pippin"];
+const birthday = "Bilbo Baggins' birthday"
+
+function writeCards(names, event){
+    let hobbitMessages = [];
+    for (let i = 0; i < names.length; i++){
+        hobbitMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    
+    return hobbitMessages
+}
+
+console.log(writeCards(hobbits, birthday));
+
+/*while ([condition]) {
+  [loop body]
+} 
+*/
+
+function countDown(seconds){
+    while (seconds>=0){
+        console.log(seconds--);
+    }
+}
+
+countDown(10);
